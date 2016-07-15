45a27f2641ae84694f9ad032cdc266b5759f9aa2
Derick Quartey <niikwartey1@gmail.com>
2016-07-15 14:21:49 -0400

    finished cash register lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,6 @@
 class CashRegister
-  attr_accessor :total, :discount, :items
-    
-    @last_item_total = 0
+  attr_accessor :total, :items
+  attr_reader :discount, :last_item_total
 
   def initialize(discount = nil)
     @discount = discount
@@ -9,34 +8,26 @@ class CashRegister
     @items = []
   end
 
-  def total
-    @total
-  end
-
   def add_item(item, price, quantity = 1)
     item_total = quantity * price
-    @total += item_total
+    self.total += item_total
     # add to record
     quantity.times do 
-      @items << item
+      self.items << item
     end
     @last_item_total = item_total
   end
 
   def apply_discount
     if @discount
-      @total -= (@total * discount)/100
+      self.total -= (self.total * discount)/100
       "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
     end
   end
 
-  def items
-    @items
-  end
-
   def void_last_transaction
-    @total -= @last_item_total
+    self.total -= self.last_item_total
   end
 end
